c6e3c241e34e74207154f257cb99088d595d29f1
CowKeyMan <dancauchi1@gmail.com>
2022-03-07 14:29:00 +0200

    Add samples metrics when printing metrics

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -15,6 +15,10 @@ def print_metrics_multilabel(target, output):
         f'{precision_score(target, output, average="micro", zero_division=0)}'
     )
     print(
+        '\tSamples Precision: '
+        f'{precision_score(target, output, average="samples", zero_division=0)}'
+    )
+    print(
         '\tMacro Recall: '
         f'{recall_score(target, output, average="macro", zero_division=0)}'
     )
@@ -23,10 +27,18 @@ def print_metrics_multilabel(target, output):
         f'{recall_score(target, output, average="micro", zero_division=0)}'
     )
     print(
+        '\tSamples Recall: '
+        f'{recall_score(target, output, average="samples", zero_division=0)}'
+    )
+    print(
         '\tMacro F1: '
         f'{f1_score(target, output, average="macro", zero_division=0)}'
     )
     print(
         '\tMicro F1: '
         f'{f1_score(target, output, average="micro", zero_division=0)}'
+    )
+    print(
+        '\tSamples F1: '
+        f'{f1_score(target, output, average="samples", zero_division=0)}'
     )
