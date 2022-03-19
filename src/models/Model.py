63d826cdb036898275ad0345093c6c127331a2cf
CowKeyMan <dancauchi1@gmail.com>
2022-03-19 13:23:54 +0200

    Add non-interactive training or the model

--- src/models/Model.py
+++ src/models/Model.py
@@ -21,19 +21,21 @@ class Model(nn.Module):
         train_loader: torch.utils.data.DataLoader,
         dev_loader: torch.utils.data.DataLoader,
         verbose: int = 1,
+        interactive=False,
     ) -> None:
         train_losses = []
         dev_losses = []
         dev_batches = []
         dev_loss_lower_than_min_consecutive_count = 0
-        plt.ion()
-        figure, axs = plt.subplots(1, 2)
-        train_line, = axs[0].plot(range(10), range(10), label='train_loss')
-        axs[0].set_title('Train Loss')
-        dev_line, = axs[1].plot(dev_batches, dev_losses, label='dev_loss')
-        axs[1].set_title('Development Loss')
+        if interactive:
+            plt.ion()
+            figure, axs = plt.subplots(1, 2)
+            train_line, = axs[0].plot(range(10), range(10), label='train_loss')
+            axs[0].set_title('Train Loss')
+            dev_line, = axs[1].plot(dev_batches, dev_losses, label='dev_loss')
+            axs[1].set_title('Development Loss')
         min_loss = float('inf')
-        for epoch in range(n_epochs):
+        for epoch in range(2):
             self.train()  # set model to training mode
             for batch_num, (data, target) in enumerate(train_loader):
                 data, target = data.to(device), target.to(device)
@@ -63,12 +65,15 @@ class Model(nn.Module):
                         target.cpu().numpy(), predicted.cpu().numpy()
                     )
 
-                train_line.set_xdata(range(len(train_losses)))
-                train_line.set_ydata(train_losses)
-                axs[0].set_xlim(0, len(train_losses))
-                axs[0].set_ylim(0, max(train_losses) * 1.2)
-                figure.canvas.draw()
-                figure.canvas.flush_events()
+                if interactive:
+                    train_line.set_xdata(range(len(train_losses)))
+                    train_line.set_ydata(train_losses)
+                    axs[0].set_xlim(0, len(train_losses))
+                    axs[0].set_ylim(0, max(train_losses) * 1.2)
+                    figure.canvas.draw()
+                    figure.canvas.flush_events()
+
+                if batch_num > 2: break
 
             with torch.no_grad():
                 self.eval()  # set model to eval mode
@@ -87,12 +92,13 @@ class Model(nn.Module):
                 loss_total = loss_total.item() / len(dev_loader)
                 dev_losses.append(loss_total)
                 dev_batches.append(len(train_losses))
-                axs[1].set_xlim(0, dev_batches[-1] * 1.2)
-                axs[1].set_ylim(0, max(dev_losses) * 1.2)
-                dev_line.set_xdata(dev_batches)
-                dev_line.set_ydata(dev_losses)
-                figure.canvas.draw()
-                figure.canvas.flush_events()
+                if interactive:
+                    axs[1].set_xlim(0, dev_batches[-1] * 1.2)
+                    axs[1].set_ylim(0, max(dev_losses) * 1.2)
+                    dev_line.set_xdata(dev_batches)
+                    dev_line.set_ydata(dev_losses)
+                    figure.canvas.draw()
+                    figure.canvas.flush_events()
 
                 if verbose > 0:
                     print(
@@ -112,5 +118,16 @@ class Model(nn.Module):
                     dev_loss_lower_than_min_consecutive_count += 1
                     if dev_loss_lower_than_min_consecutive_count == 3:
                         break
-        plt.ioff()
+
+        if interactive:
+            plt.ioff()
+        else:
+            figure, axs = plt.subplots(1, 2)
+            top = max(max(train_losses), max(dev_losses)) * 1.2
+            axs[0].plot(range(len(train_losses)), train_losses, label='train_loss')
+            axs[0].set_title('Train Loss')
+            axs[0].set_ylim(bottom=0, top=top)
+            axs[1].plot(dev_batches, dev_losses, label='dev_loss')
+            axs[1].set_title('Development Loss')
+            axs[1].set_ylim(bottom=0, top=top)
         plt.savefig('resources/models/Transferred.png')
