af46fdcd837694d548d3128b00815c9c804811dc
outisa <savolainen.outi@gmail.com>
2022-03-19 09:46:59 +0200

    Test cnn with more channels

--- src/models/Model.py
+++ src/models/Model.py
@@ -7,7 +7,6 @@ import torch
 from matplotlib import pyplot as plt
 from utils.metrics import print_metrics_multilabel
 
-
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
@@ -26,12 +25,12 @@ class Model(nn.Module):
         dev_losses = []
         dev_batches = []
         dev_loss_lower_than_min_consecutive_count = 0
-        plt.ion()
-        figure, axs = plt.subplots(1, 2)
-        train_line, = axs[0].plot(range(10), range(10), label='train_loss')
-        axs[0].set_title('Train Loss')
-        dev_line, = axs[1].plot(dev_batches, dev_losses, label='dev_loss')
-        axs[1].set_title('Development Loss')
+        #plt.ion()
+        #figure, axs = plt.subplots(1, 2)
+        #train_line, = axs[0].plot(range(10), range(10), label='train_loss')
+        #axs[0].set_title('Train Loss')
+        #dev_line, = axs[1].plot(dev_batches, dev_losses, label='dev_loss')
+        #axs[1].set_title('Development Loss')
         min_loss = float('inf')
         for epoch in range(n_epochs):
             self.train()  # set model to training mode
@@ -63,12 +62,12 @@ class Model(nn.Module):
                         target.cpu().numpy(), predicted.cpu().numpy()
                     )
 
-                train_line.set_xdata(range(len(train_losses)))
-                train_line.set_ydata(train_losses)
-                axs[0].set_xlim(0, len(train_losses))
-                axs[0].set_ylim(0, max(train_losses) * 1.2)
-                figure.canvas.draw()
-                figure.canvas.flush_events()
+                #train_line.set_xdata(range(len(train_losses)))
+                #train_line.set_ydata(train_losses)
+                #axs[0].set_xlim(0, len(train_losses))
+                #axs[0].set_ylim(0, max(train_losses) * 1.2)
+                #figure.canvas.draw()
+                #figure.canvas.flush_events()
 
             with torch.no_grad():
                 self.eval()  # set model to eval mode
@@ -86,12 +85,12 @@ class Model(nn.Module):
 
                 dev_losses.append(loss_total.item())
                 dev_batches.append(len(train_losses))
-                axs[1].set_xlim(0, dev_batches[-1] * 1.2)
-                axs[1].set_ylim(0, max(dev_losses) * 1.2)
-                dev_line.set_xdata(dev_batches)
-                dev_line.set_ydata(dev_losses)
-                figure.canvas.draw()
-                figure.canvas.flush_events()
+                #axs[1].set_xlim(0, dev_batches[-1] * 1.2)
+                #axs[1].set_ylim(0, max(dev_losses) * 1.2)
+                #dev_line.set_xdata(dev_batches)
+                #dev_line.set_ydata(dev_losses)
+                #figure.canvas.draw()
+                #figure.canvas.flush_events()
 
                 if verbose > 0:
                     print(
@@ -111,5 +110,15 @@ class Model(nn.Module):
                     dev_loss_lower_than_min_consecutive_count += 1
                     if dev_loss_lower_than_min_consecutive_count == 3:
                         break
-        plt.ioff()
-        plt.show(block=True)
+        #plt.ioff()
+        #plt.show(block=True)
+        #Development: Epoch 30: Loss: 9.6065
+        #Macro Precision: 0.4025974025974026
+        #Micro Precision: 0.7049180327868853
+        #Samples Precision: 0.6064814814814814
+        #Macro Recall: 0.2800865800865801
+        #Micro Recall: 0.6142857142857143
+        #Samples Recall: 0.5185185185185185
+        #Macro F1: 0.314787784679089
+        #Micro F1: 0.6564885496183206
+        #Samples F1: 0.5272486772486773
--- src/utils/image_loader.py
+++ src/utils/image_loader.py
@@ -7,18 +7,18 @@ from torch.utils.data import DataLoader
 def get_dataloaders(df, test_size, img_dir, batch_size_train, batch_size_test):
     colour_jitter = 0.1
     train_transform = transforms.Compose([
-        # transforms.RandomAdjustSharpness(2),
-        # transforms.ColorJitter(
-        #     brightness=colour_jitter,
-        #     contrast=colour_jitter,
-        #     saturation=colour_jitter,
-        #     hue=colour_jitter,
-        # ),
-        # transforms.GaussianBlur(kernel_size=3, sigma=(0.01, 4)),
+        transforms.RandomAdjustSharpness(2),
+        transforms.ColorJitter(
+            brightness=colour_jitter,
+            contrast=colour_jitter,
+            saturation=colour_jitter,
+            hue=colour_jitter,
+        ),
+        transforms.GaussianBlur(kernel_size=3, sigma=(0.01, 4)),
         transforms.RandomHorizontalFlip(),
-        # transforms.RandomVerticalFlip(),
-        # transforms.RandomAffine(degrees=180),
-        # transforms.RandomPerspective(),
+        transforms.RandomVerticalFlip(),
+        #transforms.RandomAffine(degrees=180),
+        transforms.RandomPerspective(),
         transforms.RandomGrayscale(),
         transforms.ToTensor(),
     ])
