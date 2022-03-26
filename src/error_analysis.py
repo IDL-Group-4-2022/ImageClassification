e92bf30fd0b45f34003352fffde1285724fb0acb
CowKeyMan <dancauchi1@gmail.com>
2022-03-26 11:59:04 +0200

    Allow multiple images to be predicted in the error analysis

--- src/error_analysis.py
+++ src/error_analysis.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 
 
 def predict_image(
-    images_path: Path, image_id: int, model_path: Path, df: pd.DataFrame
+    images_path: Path, image_ids: list[int], model_path: Path, df: pd.DataFrame
 ) -> None:
     if torch.cuda.is_available():
         device = torch.device('cuda')
@@ -17,28 +17,33 @@ def predict_image(
     model = torch.load(model_path).to(device)
     model.eval()
     labels = df.columns
-    image = Image.open(f"{images_path}im{image_id}.jpg").convert("RGB")
-    tensor = transforms.ToTensor()(image).unsqueeze_(0).to(device)
-    predictions = np.where(torch.sigmoid(model(tensor)).cpu() > 0.5, 1, 0)[0]
-    actual = df.loc[image_id]
-    print(
-        "actual: "
-        f"{[labels[i] for i, onehot in enumerate(actual) if onehot == 1]}"
-    )
-    print(
-        "Predicted: "
-        f"{[labels[i] for i, onehot in enumerate(predictions) if onehot == 1]}"
-    )
-    plt.imshow(image)
-    plt.show()
+    for image_id in image_ids:
+        image = Image.open(f"{images_path}im{image_id}.jpg").convert("RGB")
+        tensor = transforms.ToTensor()(image).unsqueeze_(0).to(device)
+        predictions = np.where(torch.sigmoid(model(tensor)).cpu() > 0.5, 1, 0)[0]
+        if image_id in df.index:
+            actual = df.loc[image_id]
+        else:
+            actual = [0] * len(df.columns)
+        print(
+            "actual: "
+            f"{[labels[i] for i, onehot in enumerate(actual) if onehot == 1]}"
+        )
+        print(
+            "Predicted: "
+            f"{[labels[i] for i, onehot in enumerate(predictions) if onehot == 1]}"
+        )
+        plt.axis('off')
+        plt.imshow(image)
+        plt.show()
 
 
 def test_predict_image():
     predict_image(
         'resources/data/original/dl2021-image-corpus-proj/images/',
-        200,
+        [1, 41, 43, 55, 91, 94, 135, 200, 283, 509, 613],
         'resources/models/Cnn2_final.pytorch',
-        pd.read_csv('resources/data/generated/train.csv',index_col='im_name'),
+        pd.read_csv('resources/data/generated/train.csv', index_col='im_name'),
     )
 
 
